083db942f4b1ca9ed48810add696d328b37d266b
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-08 14:56:22 +0100

    Completes B_Teams, all tests passed

--- B_teams/specs/teams_class_spec.rb
+++ B_teams/specs/teams_class_spec.rb
@@ -20,9 +20,36 @@ class TestTeams < MiniTest::Test
     assert_equal('Wenger', @team1.coach())
   end
 
+  def test_get_points
+    assert_equal(0, @team1.points)
+  end
+
   def test_set_coach
     @team1.coach = 'Emery'
     assert_equal('Emery', @team1.coach())
+  end
+
+  def test_add_player
+    @team1.add_player('Sanchez')
+    assert_equal('Sanchez', @team1.players[4])
+  end
+
+  def test_include_player__true
+    assert_equal(true, @team1.include_player?('Henry'))
+  end
+
+  def test_include_player__false
+    assert_equal(false, @team1.include_player?('Aguero'))
+  end
+
+  def test_update_points__win
+    @team1.update_points('win')
+    assert_equal(3, @team1.points)
+  end
+
+  def test_update_points__lose
+    @team1.update_points('lose')
+    assert_equal(0, @team1.points)
   end
 
 end
--- B_teams/teams_class.rb
+++ B_teams/teams_class.rb
@@ -11,15 +11,26 @@
 
 class Team
 
-  attr_reader :team_name, :players
+  attr_reader :team_name, :players, :points
   attr_accessor :coach
 
   def initialize(input_team_name, input_players, input_coach)
     @team_name = input_team_name
     @players = input_players
     @coach = input_coach
+    @points = 0
   end
 
+  def add_player(new_player)
+    @players << new_player
+  end
 
+  def include_player?(player)
+    @players.include?(player) ? true : false
+  end
+
+  def update_points(match_result)
+    @points += 3 if match_result == 'win'
+  end
 
 end
