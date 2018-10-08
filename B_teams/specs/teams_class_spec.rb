1a5475c266c79014b96ae6a28068b015168183b6
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-08 14:25:10 +0100

    Refactors B_Teams to use attr_reader and attr_accessor

--- B_teams/specs/teams_class_spec.rb
+++ B_teams/specs/teams_class_spec.rb
@@ -9,20 +9,20 @@ class TestTeams < MiniTest::Test
   end
 
   def test_get_team_name
-    assert_equal('Arsenal', @team1.get_team_name)
+    assert_equal('Arsenal', @team1.team_name())
   end
 
   def test_get_players
-    assert_equal(['Henry', 'Bergkamp', 'Adams', 'Vieira'], @team1.get_players)
+    assert_equal(['Henry', 'Bergkamp', 'Adams', 'Vieira'], @team1.players())
   end
 
   def test_get_coach
-    assert_equal('Wenger', @team1.get_coach)
+    assert_equal('Wenger', @team1.coach())
   end
 
   def test_set_coach
-    @team1.set_coach('Emery')
-    assert_equal('Emery', @team1.get_coach)
+    @team1.coach = 'Emery'
+    assert_equal('Emery', @team1.coach())
   end
 
 end
--- B_teams/teams_class.rb
+++ B_teams/teams_class.rb
@@ -11,26 +11,15 @@
 
 class Team
 
+  attr_reader :team_name, :players
+  attr_accessor :coach
+
   def initialize(input_team_name, input_players, input_coach)
     @team_name = input_team_name
     @players = input_players
     @coach = input_coach
   end
 
-  def get_team_name
-    return @team_name
-  end
 
-  def get_players
-    return @players
-  end
-
-  def get_coach
-    return @coach
-  end
-
-  def set_coach(new_coach)
-    @coach = new_coach
-  end
 
 end
