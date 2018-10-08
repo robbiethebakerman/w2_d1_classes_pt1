39d88afc9fded5bfa73f81e3b0d4e748da7c7022
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-08 13:19:19 +0100

    Initial commit

---
+++ A_students/specs/students_class_spec.rb
@@ -0,0 +1,15 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../students_class')
+
+class TestStudents < MiniTest::Test
+
+  def setup
+
+  end
+
+  def test_1
+
+  end
+
+end
